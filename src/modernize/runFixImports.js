f769176d3ef37ae872ba6793cb1791026f9c4272
Marie Chatfield <marie@pingboard.com>
2019-06-05 15:44:29 -0500

    Update coffeescript require statements when now accessing ES6 default exports

--- src/modernize/runFixImports.js
+++ src/modernize/runFixImports.js
@@ -2,13 +2,16 @@
  * Runs the fix-imports step on all specified JS files, and return an array of
  * the files that changed.
  */
-import { readFile } from 'fs-promise';
+import { readFile, writeFile } from 'fs-promise';
 import { basename, join, relative, resolve } from 'path';
 import zlib from 'zlib';
 
 import runWithProgressBar from '../runner/runWithProgressBar';
 import execLive from '../util/execLive';
 import getFilesUnderPath from '../util/getFilesUnderPath';
+
+const JS_OR_JSX_EXTENSION = /\.jsx?$/;
+const COFFEE_EXTENSION = /\.coffee$/;
 
 export default async function runFixImports(jsFiles, config) {
   let {searchPath, absoluteImportPaths, jscodeshiftScripts} = config.fixImportsConfig;
@@ -25,7 +28,10 @@ export default async function runFixImports(jsFiles, config) {
     absoluteImportPaths: absoluteImportPaths.map(p => resolve(p)),
   };
   let eligibleFixImportsFiles = await getEligibleFixImportsFiles(
-    config, searchPath, jsFiles);
+    config, searchPath, jsFiles, JS_OR_JSX_EXTENSION);
+  let eligibleFixImportsCoffeeFiles = await getEligibleFixImportsFiles(
+    config, searchPath, jsFiles, COFFEE_EXTENSION);
+
   console.log('Fixing any imports across the whole codebase...');
   if (eligibleFixImportsFiles.length > 0) {
     // Note that the args can get really long, so we take reasonable steps to
@@ -45,13 +51,22 @@ export default async function runFixImports(jsFiles, config) {
           ${eligibleRelativePaths.join(' ')} --encoded-options=${encodedOptions}`);
     });
   }
-  return eligibleFixImportsFiles;
+
+  if (eligibleFixImportsCoffeeFiles.length > 0) {
+    console.log(`Fixing imports for ${eligibleFixImportsCoffeeFiles.length} coffeescript files...`);
+
+    const convertedFilesWithDefaultExports = getFilesWithDefaultExports(options);
+
+    eligibleFixImportsCoffeeFiles.forEach(filePath => fixImportsForCoffeeScript(filePath, convertedFilesWithDefaultExports));
+  }
+
+  return [...eligibleFixImportsFiles, ...eligibleFixImportsCoffeeFiles];
 }
 
-async function getEligibleFixImportsFiles(config, searchPath, jsFiles) {
+async function getEligibleFixImportsFiles(config, searchPath, jsFiles, extension) {
   let jsBasenames = jsFiles.map(p => basename(p, '.js'));
   let resolvedPaths = jsFiles.map(p => resolve(p));
-  let allJsFiles = await getFilesUnderPath(searchPath, p => p.endsWith('.js') || p.endsWith('.jsx'));
+  let allJsFiles = await getFilesUnderPath(searchPath, p => p.match(extension));
   await runWithProgressBar(
     config,
     'Searching for files that may need to have updated imports...',
@@ -83,4 +98,38 @@ function resolveJscodeshiftScriptPath(scriptPath) {
     return join(__dirname, `../jscodeshift-scripts-dist/${scriptPath}`);
   }
   return scriptPath;
+}
+
+function getFilesWithDefaultExports(options) {
+  const {convertedFiles} = options;
+
+  return convertedFiles.filter(async (filePath) => {
+    const contents = (await readFile(resolve(filePath))).toString();
+
+    return /\nexport default/.test(contents);
+  });
+}
+
+// Any files that were converted and now have a default export *instead of*
+// a commonjs module.export should now update any remaining coffeescript files
+// which require them.
+//
+// Those requires statements should change from `require("../foo")` to
+// `require("../foo").default` to properly access the new ES6 exports.
+async function fixImportsForCoffeeScript(filePath, convertedFiles) {
+  const jsBasenames = convertedFiles.map(p => basename(p, '.js'));
+
+  const resolvedPath = resolve(filePath);
+
+  let contents = (await readFile(resolvedPath)).toString();
+  for (let jsBasename of jsBasenames) {
+    const requireFilePattern = new RegExp(`\\w+ = require\\("[a-z\\-\\.\\/]+${jsBasename}"\\)`);
+    const hasAMatch = contents.match(requireFilePattern);
+    if (hasAMatch) {
+      const requireLine = hasAMatch[0];
+      contents = contents.replace(requireLine, `${requireLine}.default`);
+    }
+  }
+
+  await writeFile(resolvedPath, contents);
 }
